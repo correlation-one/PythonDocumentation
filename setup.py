60824226f072e03ecbc12b529e679f8de8431823
Ryan McPartlan <ryan@correlation-one.com>
2018-09-18 15:44:27 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PythonDocumentation',
+    version='1.0',
+    packages=['PythonSphinxDocs.gamelib'],
+    url='',
+    license='',
+    author='Correlation One',
+    author_email='',
+    description=''
+)
